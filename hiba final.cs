d13c02ae3996f41525b95390c8391473bc35c1d9
Hiba-sidd99 <38987448+Hiba-sidd99@users.noreply.github.com>
2021-11-30 12:26:35 +0500

    Add files via upload

---
+++ hiba final.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class MONTHLYEMP
+
+    {
+        int basePay;
+        int hourlyPayRate;
+        int overtimeHours;
+        int overtimePay;
+        int unitPay;
+        int Grosspay;
+        int hoursWorked;
+        int BASE_HOURS;
+        int OVERTIME_RATE;
+        public void Calculate_Salary()
+        {
+            if(hoursWorked > BASE_HOURS)
+            { 
+            basePay = hourlyPayRate * BASE_HOURS;
+            overtimeHours = hoursWorked - BASE_HOURS;
+            overtimePay = overtimeHours * hourlyPayRate * OVERTIME_RATE;
+            unitPay = 3 * unit produced;
+            Grosspay = basePay + overtimePay + unitPay
+        }
+    }
+}
